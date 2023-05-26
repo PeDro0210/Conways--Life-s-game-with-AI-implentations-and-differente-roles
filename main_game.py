9fdf526ead25208e34b9d0ee41e31c7861a04361
PeDro0210 <pedroruben0210@gmail.com>
2023-05-25 22:17:35 -0600

    just one step closer to making the game loop

--- main_game.py
+++ main_game.py
@@ -2,7 +2,6 @@ import pygame
 from pygame.locals import *
 from dot_classes import reddot, bluedot
 from game_classes import MiniGrid, button, game_of_life  
-import os
 
 grid= MiniGrid(10, 1920/16, 1080/8)
 window=pygame.display.set_mode((1920,1080))
@@ -64,54 +63,74 @@ while True:
                 game.neighboar_checking() #dude, leave this here, it would start with the main loop    
                 #I was just trying how did the state atributes worked, it worked pretty well
 
-
+            
+                new_gen_blue=[]
+                new_gen_coords_blue=[]
+                new_gen_red=[]
+                new_gen_coords_red=[]
 
                 for b_dot, r_dot in zip(game.blue_dots, game.red_dots):
                     
-                
                     
-                       
+                    
                     
                     intersection=game.inteserction_betwean_dots_blue(b_dot)     
                     intersection_r=game.inteserction_betwean_dots_red(r_dot)
                     
-                    
                     if None != intersection: 
-                        print(intersection) 
-                        new_dot_b=bluedot(intersection[0],intersection[1],0,0,255,4,True,[],[],[])
-                        print(new_dot_b.x,new_dot_b.y)
-                        game.blue_dots.append(new_dot_b)
-                        game.blue_coords.append([new_dot_b.x,new_dot_b.y])
-                        draw=new_dot_b.draw(window)
-                        print(draw)
+                        for j in intersection:
+                            new_dot_b=bluedot(j[0],j[1],0,0,255,4,True,[],[],[])
+                            print(new_dot_b.x,new_dot_b.y)
+                            new_gen_blue.append(new_dot_b)
+                            new_gen_coords_blue.append([new_dot_b.x,new_dot_b.y])
+                            draw=new_dot_b.draw(window)
                             
                     
                     if None != intersection_r:
-                        print(intersection_r)
-                        new_dot_r=reddot(intersection_r[0],intersection_r[1],255,0,0,4,True,[],[],[])
-                        print(new_dot_r.x,new_dot_r.y)
-                        game.red_dots.append(new_dot_r)
-                        game.red_coords.append([new_dot_r.x,new_dot_r.y])
-                        draw_2=new_dot_r.draw(window)
-                        print(draw_2)
-                    
-                    
-                    # b_dot.point_state()#this is conflicting with the new generations
-                    # r_dot.point_state()#this is conflicting with the new generations
-             
+                        for i in intersection_r:
+                            new_dot_r=reddot(i[0],i[1],255,0,0,4,True,[],[],[])
+                            print(new_dot_r.x,new_dot_r.y)
+                            new_gen_red.append(new_dot_r)
+                            new_gen_coords_red.append([new_dot_r.x,new_dot_r.y])
+                            draw_2=new_dot_r.draw(window)
+
+                    b_dot.point_state()
+                    r_dot.point_state()
+            
 
                     if b_dot.state:
                         pass
                     else:
-                        game.blue_coords.remove([b_dot.x,b_dot.y])
-                        b_dot.remove(window)
+                        if [b_dot.x,b_dot.y] in game.blue_coords:
+                            game.blue_coords.remove([b_dot.x,b_dot.y])
+                            b_dot.remove(window)
+                        else:
+                            pass
                         
                     
                     if r_dot.state:
                         pass
                     else:
-                        game.red_coords.remove([r_dot.x,r_dot.y])
-                        r_dot.remove(window)
+                        if [r_dot.x,r_dot.y] in game.red_coords:
+                            game.red_coords.remove([r_dot.x,r_dot.y])
+                            r_dot.remove(window)
+                        else:
+                            pass
+
+                for new_blue, new_b_coords in zip(new_gen_blue, new_gen_coords_blue):
+                    game.blue_dots.append(new_blue)
+                    game.blue_coords.append(new_b_coords)
+                
+                for new_red, new_r_coords in zip(new_gen_red, new_gen_coords_red):
+                    game.red_dots.append(new_red)
+                    game.red_coords.append(new_r_coords)
+                    
+
+                    
+                    
+
+
+
 
                         
 
